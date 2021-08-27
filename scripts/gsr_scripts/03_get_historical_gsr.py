e274f6782d8a9ac9e3ea62c34ee268b57a2cc307
Hilding Wollbo <wollbo@kth.se>
2021-08-27 22:44:22 +0200

    added functionality for reading and concatenating existing old data

--- scripts/gsr_scripts/03_get_historical_gsr.py
+++ scripts/gsr_scripts/03_get_historical_gsr.py
@@ -3,20 +3,29 @@ from brownie import GSRConsumer
 import pandas as pd
 
 
-window = 60
+window = 100
 
 def find_window_entries(df, query='get_historical_xau', window=100):
     """Returns df with the latest len(window) values for the selected query"""
     price_feed_contract = GSRConsumer[-1]
-    print(df['round_id'].iloc[-1])
+    try:
+        df_old = pd.read_csv(f'data/{query.rsplit("_")[-1]}_{window}')
+        first_round = int(df_old["round_id"][0])
+        print('using old data')
+    except:
+        print('no old data')
+        first_round = 0
     i = 1  # round_ids are not necessarily incremental
-    while len(df) < window:
+    tup = (df['round_id'].iloc[-1],)
+    while len(df) < window and first_round < tup[0]:
         tup = getattr(price_feed_contract, query)(df['round_id'].iloc[-1]-i)
         if tup[3]:  # time_stamp != 0
             df = df.append(pd.DataFrame([tup], columns=df.columns))
             i = 1
         else:
             i += 1
+    if tup[0] <= first_round:
+        df = df.append(df_old[1:window-len(df)])
     return df.reset_index(drop=True)
 
 
